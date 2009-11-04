e2c85fbb8f02c92594a8e1bcb4864b6f1d0f2b13
Andrew Waterman <waterman@r53.millennium.berkeley.edu>
2009-11-03 22:26:15 -0800

    Added changes to support C++ apps
    Specifically, static initialization and destruction.  Need to
    provide to g++ a __CTOR_LIST__ and __DTOR_LIST__ section in
    the binary.  Also need to call all the CTORs before entering
    main(), and need to use atexit() to call all the DTORs on exit.

--- user/parlib/src/parlibmain.c
+++ user/parlib/src/parlibmain.c
@@ -5,8 +5,34 @@
 #include <unistd.h>
 #include <debug.h>
 
+// call static destructors.
+void parlib_dtors()
+{
+	typedef void (*dtor)(void);
+	extern dtor __DTOR_LIST__[], __DTOR_END__[];
+
+	for(int i = 0; i < __DTOR_END__ - __DTOR_LIST__; i++)
+		__DTOR_LIST__[i]();
+}
+
+// call static constructors.
+void parlib_ctors()
+{
+	typedef void (*ctor)(void);
+	extern ctor __CTOR_LIST__[],__CTOR_END__[];
+
+	for(int i = -1; i >= __CTOR_LIST__-__CTOR_END__; i--)
+		__CTOR_END__[i]();
+}
+
 void parlibmain()
 {
+	// call static destructors
+	parlib_ctors();
+
+	// call static destructors on exit
+	atexit(&parlib_dtors);
+
 	// call user main routine
 	extern int main(int argc, char * NTS * COUNT(argc) NT argv);
 	int r = main(procinfo.argc, procinfo.argv);
