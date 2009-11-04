34c2a615fefb75c9a092216e2f0bb441b9611ca1
Andrew Waterman <waterman@r53.millennium.berkeley.edu>
2009-11-03 22:30:44 -0800

    Made static destructors thread-safe

--- user/parlib/src/parlibmain.c
+++ user/parlib/src/parlibmain.c
@@ -4,12 +4,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <debug.h>
+#include <hart.h>
 
 // call static destructors.
 void parlib_dtors()
 {
 	typedef void (*dtor)(void);
 	extern dtor __DTOR_LIST__[], __DTOR_END__[];
+
+	// make sure only one thread actually runs the dtors
+	static size_t already_done = 0;
+	if(hart_swap(&already_done,1) == 1)
+		return;
 
 	for(int i = 0; i < __DTOR_END__ - __DTOR_LIST__; i++)
 		__DTOR_LIST__[i]();
