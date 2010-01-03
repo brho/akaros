6e624068aebb73037e7290b1ca2151e4c8110ce4
Andrew Waterman <waterman@ubuntu.(none)>
2010-01-02 17:07:51 -0800

    Changed argc/argv/envp ABI

--- kern/include/ros/procdata.h
+++ kern/include/ros/procdata.h
@@ -9,14 +9,17 @@
 #include <ros/error.h>
 #include <ros/common.h>
 
-#define PROCINFO_MAX_ARGV_SIZE 2048
+#define PROCINFO_MAX_ARGV_SIZE 1024
+#define PROCINFO_MAX_ENV_SIZE 1024
 
 typedef struct procinfo {
 	pid_t pid;
+	//pid_t ppid;
 	size_t max_harts;
 	uint64_t tsc_freq;
 
 	char argv_buf[PROCINFO_MAX_ARGV_SIZE];
+	char env_buf[PROCINFO_MAX_ENV_SIZE];
 } procinfo_t;
 #define PROCINFO_NUM_PAGES  ((sizeof(procinfo_t)-1)/PGSIZE + 1)	
 
