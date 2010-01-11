72b3244b85cbfc08e81313ee46e69e71e27ad887
Barret Rhoden <brho@cs.berkeley.edu>
2010-01-11 13:15:43 -0800

    Fixes procdata's ring buffers
    Ring buffers need their space to be allocated directly after the sring
    struct.

--- kern/include/ros/procdata.h
+++ kern/include/ros/procdata.h
@@ -26,7 +26,9 @@ typedef struct procinfo {
 typedef struct procdata {
 	// The actual ring buffers for communicating with user space
 	syscall_sring_t  syscallring;  // Per-process ring buffer for async syscalls
+	char padding1[SYSCALLRINGSIZE - sizeof(syscall_sring_t)];
 	sysevent_sring_t syseventring; // Per-process ring buffer for async sysevents
+	char padding2[SYSEVENTRINGSIZE - sizeof(sysevent_sring_t)];
 #ifdef __i386__
 	segdesc_t *ldt;
 #endif
