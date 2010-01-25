015faf9080df5d58f234f02ce5052b92d0b6cf80
Barret Rhoden <brho@cs.berkeley.edu>
2010-01-25 12:17:24 -0800

    Fixes some syscall issues, esp getpid
    Adds sys_getpid to the syscall table, provides return values for calls
    like proc_yield, keeps the table entries in the order in ros/syscall.h,
    and reorganizes the syscalls into groups.

--- kern/include/ros/syscall.h
+++ kern/include/ros/syscall.h
@@ -5,18 +5,24 @@
 #define SYS_null					 1
 #define SYS_cache_buster			 2
 #define SYS_cache_invalidate		 3
-#define SYS_cputs					 4
-#define SYS_cgetc					 5
-#define SYS_getcpuid				 6
-#define SYS_getpid					 7
-#define SYS_proc_destroy			 8
-#define SYS_shared_page_alloc		 9
-#define SYS_shared_page_free		10
-#define SYS_yield					11
-#define SYS_proc_create				12
-#define SYS_proc_run				13
-#define SYS_mmap					14
-#define SYS_brk						15
+#define SYS_reboot					 4
+#define SYS_cputs					 5
+#define SYS_cgetc					 6
+#define SYS_getcpuid				 7
+#define SYS_getvcoreid				 8
+#define SYS_getpid					 9
+#define SYS_proc_create				10
+#define SYS_proc_run				11
+#define SYS_proc_destroy			12
+#define SYS_yield					13
+#define SYS_run_binary				14
+#define SYS_fork					15
+#define SYS_exec					16
+#define SYS_trywait					17
+#define SYS_mmap					18
+#define SYS_brk						19
+#define SYS_shared_page_alloc		20
+#define SYS_shared_page_free		21
 /*
 #define SYS_mprotect
 #define SYS_mremap
@@ -25,28 +31,13 @@
 #define SYS_mlock
 #define SYS_msync
 */
-#define SYS_resource_req			16
-/* Read and write buffers over the serial port */
-#define SYS_serial_write			17
-#define SYS_serial_read				18
-/* The next 3 syscalls go with the experimental network driver. These syscalls
- * are used by newlib_backend / our remote binary loader to pull data from /
- * put data into a buffer managed by the network driver.  These should go away
- * as things mature. */
-#define SYS_eth_read				19
-#define SYS_eth_write				20
-#define SYS_run_binary				21
-// forward a syscall to front-end machine
-#define SYS_frontend				22
-#define SYS_getvcoreid				23
-#define SYS_reboot				24
-#define SYS_fork				25
-#define SYS_exec				26
-#define SYS_trywait				27
-// Keep this in sync with the last syscall number
-#define NSYSCALLS 					27
-// syscall number starts at 1 and goes up to NSYSCALLS, without holes.
-#define INVALID_SYSCALL(syscallno) ((syscallno) > NSYSCALLS)
+#define SYS_resource_req			22
+/* Platform specific syscalls */
+#define SYS_serial_read				23
+#define SYS_serial_write			24
+#define SYS_eth_read				25
+#define SYS_eth_write				26
+#define SYS_frontend				27
 
 /* For Buster Measurement Flags */
 #define BUSTER_SHARED			0x0001
