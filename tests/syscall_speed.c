da7c423c5ceb274c8c44f79124a44c90dc92cdd1
Andrew Waterman <waterman@ros-dev.(none)>
2010-04-09 15:31:13 -0700

    Added test for kernel crossing time

---
+++ tests/syscall_speed.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <parlib.h>
+#include <arch/arch.h>
+
+int main()
+{
+	int N = 8192;
+
+	long long tsc0 = read_tsc();
+	for(int i = 0; i < N; i++)
+		ros_syscall(SYS_null,0,0,0,0,0);
+	long long tsc1 = read_tsc();
+
+	printf("tsc0 = %lld\n",tsc0);
+	printf("syscall time = %lld\n",(tsc1-tsc0)/N);
+
+	return 0;
+}
