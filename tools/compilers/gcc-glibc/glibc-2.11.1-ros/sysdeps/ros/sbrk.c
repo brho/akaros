84f2a33180a58504a78d6c84d33908c5a1f39192
David Zhu <yuzhu@cs.berkeley.edu>
2010-11-22 23:06:07 -0800

    Errno must not be touched before TCB is initialized.
    Now making sbrk ignore errno in libc.
    The alternative is to replicate the logic of sbrk for this particular
    call of sbrk, which is even uglier.
    
    Also added some documentation on errno, probably need more docs on TLS.

--- tools/compilers/gcc-glibc/glibc-2.11.1-ros/sysdeps/ros/sbrk.c
+++ tools/compilers/gcc-glibc/glibc-2.11.1-ros/sysdeps/ros/sbrk.c
@@ -20,6 +20,7 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <bits/libc-lock.h>
+#include <ros/syscall.h>
 #include <ros/memlayout.h>
 #include <ros/procinfo.h>
 #include <sys/mman.h>
@@ -45,10 +46,10 @@ __internal_setbrk (uintptr_t addr)
   {
     if(real_new_brk > BRK_END)
       return -1;
-
-    if (mmap((void*)real_brk, real_new_brk-real_brk,
+	// calling mmap directly to avoid referencing errno before it is initialized.
+    if ((void*)__ros_syscall(SYS_mmap, (void*)real_brk, real_new_brk-real_brk,
              PROT_READ | PROT_WRITE | PROT_EXEC,
-             MAP_FIXED | MAP_ANONYMOUS, -1, 0) != (void*)real_brk)
+             MAP_FIXED | MAP_ANONYMOUS, -1, 0, NULL) != (void*)real_brk)
       return -1;
   }
   else if(real_new_brk < real_brk)
