1a821f84cc74d53e96ff154bd4ea042a12637ffd
Barret Rhoden <brho@cs.berkeley.edu>
2011-07-19 11:47:15 -0700

    arch/mmu.h contents now in ros/arch/mmu.h (XCC)
    This exposes certain constants to processes that they'll need to do
    things like read page tables, handle segmentation (which we already do
    for TLS), agree with the kernel on what the page offset is, etc.  This
    will be useful later, and cleans up a couple todos.  Things were crappy
    ever since we moved just a couple items at a time to ros/, but leaving
    the rest in arch/.
    
    For now, I'll leave the arch/mmu.hs, in case we come up with something
    that should be kernel-only.
    
    If this breaks things in RISC-V, please fix but leave the macros
    exposed.  I'll be using PGOFF and PTE_ADDR later, among other things.
    
    Rebuild your cross compiler / reinstall your kernel headers.

--- kern/arch/riscv/ros/mmu.h
+++ kern/arch/riscv/ros/mmu.h
@@ -1,5 +1,11 @@
-#ifndef ROS_INC_MMU_H
-#define ROS_INC_MMU_H
+/* Contains macros and constants for the kernel VM mapping, page tables,
+ * definitions for the RISC-V MMU, etc. */
+
+#ifndef ROS_INC_ARCH_MMU_H
+#define ROS_INC_ARCH_MMU_H
+
+/* **************************************** */
+/* Kernel Virtual Memory Mapping  (not really an MMU thing) */
 
 // All physical memory mapped at this address
 #ifdef __riscv64
@@ -36,8 +42,112 @@
  * mappings, and where the dynamic mappings will start. */
 #define KERN_DYN_TOP    KERNBASE
 
+/* **************************************** */
+/* Page table constants, macros, etc */
+
 #define PGSIZE (1 << PGSHIFT)
 
+// RV64 virtual addresses are 48 bits, sign-extended out to 64 bits,
+// creating a hole between 0x0000 7FFF FFFF FFFF and 0xFFFF 8000 0000 0000.
+// Bits 11-0 are the page offset; L1/L2/L3/L4 page table indices are given
+// by bits 47-39, 38-30, 29-21, and 20-12, respectively.
+//
+// In RV32, virtual addresses are 32 bits; bits 11-0 are the page offset;
+// and L1/L2 page table indices are given by bits 31-22 and 21-12,
+// respectively.
+//
+// In both cases, the last-level page size is 4KB, as is the page table size.
+
+// page number field of address
+#define LA2PPN(la)	(((uintptr_t) (la)) >> PGSHIFT)
+
+// page number field of PPN
+#define PTE2PPN(pte)	(((uintptr_t) (pte)) >> PTE_PPN_SHIFT)
+
+// index into L1 PT
+#define L1X(la)		((((uintptr_t) (la)) >> L1PGSHIFT) & (NPTENTRIES-1))
+
+// index into L2 PT
+#define L2X(la)		((((uintptr_t) (la)) >> L2PGSHIFT) & (NPTENTRIES-1))
+
+#ifdef __riscv64
+// index into L3 PT
+#define L3X(la)		((((uintptr_t) (la)) >> L3PGSHIFT) & (NPTENTRIES-1))
+
+// index into L4 PT
+#define L4X(la)		((((uintptr_t) (la)) >> L4PGSHIFT) & (NPTENTRIES-1))
+
+// construct linear address from indexes and offset
+#define PGADDR(l1, l2, l3, l4, o) ((uintptr_t) ((l1) << L1PGSHIFT | (l2) << L2PGSHIFT | (l3) << L3PGSHIFT | (l4) << L4PGSHIFT | (o)))
+#else
+// construct linear address from indexes and offset
+#define PGADDR(l1, l2, o) ((uintptr_t) ((l1) << L1PGSHIFT | (l2) << L2PGSHIFT | (o)))
+#endif
+
+// offset in page
+#define PGOFF(la)	(((uintptr_t) (la)) & (PGSIZE-1))
+
+// construct PTE from PPN and flags
+#define PTE(ppn, flags) ((ppn) << PTE_PPN_SHIFT | (flags))
+
+// construct PTD from physical address
+#define PTD(pa) ((uintptr_t)(pa) | PTE_T)
+
+// Page directory and page table constants
+#define NPTENTRIES (PGSIZE/sizeof(pte_t))
+
+// Page table/directory entry flags.
+#define PTE_T    0x001 // Entry is a page Table descriptor
+#define PTE_E    0x002 // Entry is a page table Entry
+#define PTE_R    0x004 // Referenced
+#define PTE_D    0x008 // Dirty
+#define PTE_UX   0x010 // User eXecute permission
+#define PTE_UW   0x020 // User Read permission
+#define PTE_UR   0x040 // User Write permission
+#define PTE_SX   0x080 // Supervisor eXecute permission
+#define PTE_SW   0x100 // Supervisor Read permission
+#define PTE_SR   0x200 // Supervisor Write permission
+#define PTE_PERM (PTE_SR | PTE_SW | PTE_SX | PTE_UR | PTE_UW | PTE_UX)
+#define PTE_PPN_SHIFT 12
+
+// commly used access modes
+#define PTE_KERN_RW	(PTE_SR | PTE_SW | PTE_SX)
+#define PTE_KERN_RO	(PTE_SR | PTE_SX)
+#define PTE_USER_RW	(PTE_SR | PTE_SW | PTE_UR | PTE_UW | PTE_UX)
+#define PTE_USER_RO	(PTE_SR | PTE_UR | PTE_UX)
+
+// x86 equivalencies
+#define PTE_P      PTE_E
+#define NPDENTRIES NPTENTRIES
+#define PDX(la)    L1X(la)			// for env stuff
+
+// address in page table entry
+#define PTE_ADDR(pte)	((physaddr_t) (pte) & ~(PGSIZE-1))
+
+// address in page table descriptor
+#define PTD_ADDR(ptd)	PTE_ADDR(ptd)
+
+// MMU Control Register flags
+#define MMU_CR_E	0x00000001	// Protection Enable
+#define MMU_CR_NF	0x00000002	// No Fault mode
+#define MMU_CR_PSO	0x00000080	// Partial Store Order (TSO disabled)
+
+// MMU Fault Status Register flags
+#define MMU_FSR_USER	0x00000020	// Fault caused by user-space access
+#define MMU_FSR_EX	0x00000040	// Fault occured in instruction-space
+#define MMU_FSR_WR	0x00000080	// Fault caused by a store
+
+// MMU Register Addresses
+#define MMU_REG_CTRL	0x00000000	// MMU Control Register
+#define MMU_REG_CTXTBL	0x00000100	// MMU Context Table Pointer Register
+#define MMU_REG_CTX	0x00000200	// MMU Context Register
+#define MMU_REG_FSR	0x00000300	// MMU Fault Status Register
+#define MMU_REG_FAR	0x00000400	// MMU Fault Address Register
+
+// we must guarantee that for any PTE, exactly one of the following is true
+#define PAGE_PRESENT(pte) ((pte) & PTE_P)
+#define PAGE_UNMAPPED(pte) ((pte) == 0)
+#define PAGE_PAGED_OUT(pte) (!PAGE_PRESENT(pte) && !PAGE_UNMAPPED(pte))
 #define NOVPT
 
 #ifndef __ASSEMBLER__
@@ -45,4 +155,4 @@ typedef unsigned long pte_t;
 typedef unsigned long pde_t;
 #endif
 
-#endif
+#endif /* ROS_INC_ARCH_MMU_H */
--- kern/arch/sparc/ros/mmu.h
+++ kern/arch/sparc/ros/mmu.h
@@ -1,5 +1,11 @@
-#ifndef _ROS_ARCH_MMU_H
-#define _ROS_ARCH_MMU_H
+/* Contains macros and constants for the kernel VM mapping, page tables,
+ * definitions for the SRMMU, etc. */
+
+#ifndef ROS_INC_ARCH_MMU_H
+#define ROS_INC_ARCH_MMU_H
+
+/* **************************************** */
+/* Kernel Virtual Memory Mapping  (not really an MMU thing) */
 
 // All physical memory mapped at this address
 #define KERNBASE        0x80000000
@@ -33,4 +39,119 @@ typedef unsigned long pte_t;
 typedef unsigned long pde_t;
 #endif
 
-#endif
+/* **************************************** */
+/* Page table constants, macros, etc */
+
+// A linear address 'la' has a four-part structure as follows:
+//
+// +--------8--------+------6------+------6------+-----------12----------+
+// |  L1 Page Table  |    L2 PT    |    L3 PT    |  Offset within Page   |
+// |      Index      |    Index    |    Index    |                       |
+// +-----------------+-------------+-------------+-----------------------+
+//  \--- L1X(la) --/  \- L2X(la) -/ \- L3X(la) -/ \----- PGOFF(la) -----/
+//  \----------- PPN(la) -----------------------/
+//
+// The L1X, L2X, L3X, PGOFF, and PPN macros decompose linear addresses
+// as shown.  To construct a linear address la from L1X(la), L2X(la),
+// and PGOFF(la), use PGADDR(L1X(la), L2X(la), L3X(la), PGOFF(la)).
+
+// page number field of address
+#define LA2PPN(la)	(((uintptr_t) (la)) >> L3PGSHIFT)
+
+// page number field of PPN
+#define PTE2PPN(pte)	(((uintptr_t) (pte)) >> 8)
+
+// index into L1 PT
+#define L1X(la)		((((uintptr_t) (la)) >> L1PGSHIFT) & 0xFF)
+
+// index into L2 PT
+#define L2X(la)		((((uintptr_t) (la)) >> L2PGSHIFT) & 0x3F)
+
+// index into L3 PT
+#define L3X(la)		((((uintptr_t) (la)) >> L3PGSHIFT) & 0x3F)
+
+// offset in page
+#define PGOFF(la)	(((uintptr_t) (la)) & 0xFFF)
+
+// construct linear address from indexes and offset
+#define PGADDR(l1, l2, l3, o) ((void*SNT) ((l1) << L1PGSHIFT | (l2) << L2PGSHIFT | (l3) << L3PGSHIFT | (o)))
+
+// construct PTE from PPN and flags
+#define PTE(ppn, flags) ((ppn) << 8 | (flags))
+
+// construct PTD from physical address
+#define PTD(pa) ((pa) >> 4 | PTE_PTD)
+
+// Number of L1 page tables (contexts) the MMU can store at any time
+#define NCONTEXTS	8
+#define CONTEXT_TABLE_PAD 8 // we require NCONTEXTS+CONTEXT_TBALE_PAD % 16 == 0
+
+// Page directory and page table constants.
+#define NL3ENTRIES	64		// # entries in an L3 page table
+#define NL2ENTRIES	64		// # entries in an L2 page table
+#define NL1ENTRIES	256		// # entries in an L1 page table
+
+// Page table/directory entry flags.
+#define PTE_PTD		0x001	// Entry is a Page Table Descriptor
+#define PTE_PTE		0x002	// Entry is a Page Table Entry
+#define PTE_ACC		0x01C	// Access modes (aka permissions, see below)
+#define PTE_R		0x020	// Referenced
+#define PTE_M		0x040	// Modified
+#define PTE_C		0x080	// Cacheable
+
+// commly used access modes
+#define PTE_KERN_RW	(7 << 2)		// Kernel Read/Write
+#define PTE_KERN_RO	(6 << 2)		// Kernel Read-Only
+#define PTE_USER_RW	(3 << 2)		// Kernel/User Read/Write
+#define PTE_USER_RO	(2 << 2)		// Kernel/User Read-Only
+
+// x86 equivalencies
+#define PTE_P		PTE_PTE			// present <=> PTE
+#define PTE_PERM	PTE_ACC			// perms <=> ACC
+#define NPDENTRIES	NL1ENTRIES		// to calculate size of pgdir
+#define PDX(la)		L1X(la)			// for env stuff
+
+// +-----+-------------------+
+// |     |   Allowed Access  |
+// | ACC +------+------------+
+// |     | User | Supervisor |
+// +-----+------+------------+
+// |  0  |  R-- |  R--       |
+// |  1  |  RW- |  RW-       |
+// |  2  |  R-X |  R-X       |
+// |  3  |  RWX |  RWX       |
+// |  4  |  --X |  --X       |
+// |  5  |  R-- |  RW-       |
+// |  6  |  --- |  R-X       |
+// |  7  |  --- |  RWX       |
+// +-----+------+------------+
+
+// address in page table entry
+#define PTE_ADDR(pte)	(((physaddr_t) (pte) & ~0xFF) << 4)
+
+// address in page table descriptor
+#define PTD_ADDR(ptd)	(((physaddr_t) (ptd) & ~0x3) << 4)
+
+// MMU Control Register flags
+#define MMU_CR_E	0x00000001	// Protection Enable
+#define MMU_CR_NF	0x00000002	// No Fault mode
+#define MMU_CR_PSO	0x00000080	// Partial Store Order (TSO disabled)
+
+// MMU Fault Status Register flags
+#define MMU_FSR_USER	0x00000020	// Fault caused by user-space access
+#define MMU_FSR_EX	0x00000040	// Fault occured in instruction-space
+#define MMU_FSR_WR	0x00000080	// Fault caused by a store
+
+// MMU Register Addresses
+#define MMU_REG_CTRL	0x00000000	// MMU Control Register
+#define MMU_REG_CTXTBL	0x00000100	// MMU Context Table Pointer Register
+#define MMU_REG_CTX	0x00000200	// MMU Context Register
+#define MMU_REG_FSR	0x00000300	// MMU Fault Status Register
+#define MMU_REG_FAR	0x00000400	// MMU Fault Address Register
+
+// we must guarantee that for any PTE, exactly one of the following is true
+#define PAGE_PRESENT(pte) ((pte) & PTE_P)
+#define PAGE_UNMAPPED(pte) ((pte) == 0)
+#define PAGE_PAGED_OUT(pte) (!PAGE_PRESENT(pte) && !PAGE_UNMAPPED(pte))
+
+#endif /* ROS_INC_ARCH_MMU_H */
