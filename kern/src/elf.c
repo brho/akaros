559a33e03ea817ba553e34959a3c1dc49cda66db
Andrew Waterman <waterman@s144.Millennium.Berkeley.EDU>
2011-11-08 01:36:36 -0800

    Improved ELF loader
    - support program headers spanning more than a page
      (this does occur with 64-bit ELFs)
    
    - load program headers without MAP_FIXED, so that they
      don't collide with the dynamic linker.  we now load
      the linker starting at page 1.
    
    - fixed kernel page fault when zeroing a partial BSS
      section with demand paging.  we need to bring the
      fringe page in with MAP_POPULATE before zeroing it.

--- kern/src/elf.c
+++ kern/src/elf.c
@@ -14,7 +14,7 @@
 # define elf_field(obj, field) ((obj##32)->field)
 #endif
 
-static int load_one_elf(struct proc *p, struct file *f, int pgoffset,
+static int load_one_elf(struct proc *p, struct file *f, uintptr_t pgoffset,
                         elf_info_t *ei)
 {
 	int ret = -1;
@@ -22,42 +22,46 @@ static int load_one_elf(struct proc *p, struct file *f, int pgoffset,
 	ei->dynamic = 0;
 	ei->highest_addr = 0;
 	off_t f_off = 0;
-
-	/* assume program headers fit in a page.  if this isn't true, change the
-	 * code below that maps in program headers */
-	char* elf = (char*)kmalloc(PGSIZE, 0);
+	void* phdrs = 0;
 	
 	/* When reading on behalf of the kernel, we need to make sure no proc is
 	 * "current".  This is a bit ghetto (TODO: KFOP) */
 	struct proc *cur_proc = current;
 	current = 0;
-	if (!elf || f->f_op->read(f, elf, PGSIZE, &f_off) == -1)
-		goto fail;
-	current = cur_proc;
 
-	elf32_t* elfhdr32 = (elf32_t*)elf;
-	elf64_t* elfhdr64 = (elf64_t*)elf;
+	/* Read in ELF header. */
+	elf64_t elfhdr_storage;
+	elf32_t* elfhdr32 = (elf32_t*)&elfhdr_storage;
+	elf64_t* elfhdr64 = &elfhdr_storage;
+	if (f->f_op->read(f, (char*)elfhdr64, sizeof(elf64_t), &f_off) == -1)
+		goto fail;
+
 	bool elf32 = elfhdr32->e_ident[ELF_IDENT_CLASS] == ELFCLASS32;
-	bool elf64 = elfhdr32->e_ident[ELF_IDENT_CLASS] == ELFCLASS64;
-	if (!elf64 && !elf32)
+	bool elf64 = elfhdr64->e_ident[ELF_IDENT_CLASS] == ELFCLASS64;
+	if (elf64 == elf32)
 		goto fail;
 	#ifndef KERN64
 	if(elf64)
 		goto fail;
 	#endif
-	
-	proghdr32_t* proghdrs32 = (proghdr32_t*)(elf + elfhdr32->e_phoff);
-	proghdr64_t* proghdrs64 = (proghdr64_t*)(elf + elfhdr64->e_phoff);
-	uintptr_t e_phoff = elf_field(elfhdr, e_phoff);
+
 	size_t phsz = elf64 ? sizeof(proghdr64_t) : sizeof(proghdr32_t);
 	uint16_t e_phnum = elf_field(elfhdr, e_phnum);
-	// we don't support prog hdrs extending past the first elf page
-	if (e_phoff + e_phnum * phsz > PGSIZE)
+	uint16_t e_phoff = elf_field(elfhdr, e_phoff);
+
+	/* Read in program headers. */
+	if (e_phnum > 10000 || e_phoff % (elf32 ? 4 : 8) != 0)
+	  goto fail;
+	phdrs = kmalloc(e_phnum * phsz, 0);
+	f_off = e_phoff;
+	if (!phdrs || f->f_op->read(f, phdrs, e_phnum * phsz, &f_off) == -1)
 		goto fail;
 
+	int flags = MAP_FIXED | MAP_PRIVATE;
+
 	for (int i = 0; i < e_phnum; i++) {
-		proghdr32_t* ph32 = proghdrs32+i;
-		proghdr64_t* ph64 = proghdrs64+i;
+		proghdr32_t* ph32 = (proghdr32_t*)phdrs + i;
+		proghdr64_t* ph64 = (proghdr64_t*)phdrs + i;
 		uint16_t p_type = elf_field(ph, p_type);
 		uintptr_t p_va = elf_field(ph, p_va);
 		uintptr_t p_offset = elf_field(ph, p_offset);
@@ -66,66 +70,86 @@ static int load_one_elf(struct proc *p, struct file *f, int pgoffset,
 		uintptr_t p_filesz = elf_field(ph, p_filesz);
 
 		if (p_type == ELF_PROG_PHDR)
-			ei->phdr = elf_field(ph, p_va);
-		if (p_type == ELF_PROG_INTERP) {
-			int maxlen = MIN(PGSIZE - p_offset, sizeof(ei->interp));
-			int len = strnlen(elf + p_offset, maxlen);
-			if (len < maxlen) {
-				memcpy(ei->interp, elf + p_offset, maxlen + 1);
-				ei->dynamic = 1;
-			}
-			else
-				goto fail;
-		}
+			ei->phdr = p_va;
+		else if (p_type == ELF_PROG_INTERP) {
+			f_off = p_offset;
+			ssize_t maxlen = sizeof(ei->interp);
+			ssize_t bytes = f->f_op->read(f, ei->interp, maxlen, &f_off);
+			if (bytes == -1)
+			  goto fail;
 
-		if (p_type == ELF_PROG_LOAD && p_memsz) {
+			maxlen = MIN(maxlen, bytes);
+			if (strnlen(ei->interp, maxlen) == maxlen)
+			  goto fail;
+
+			ei->dynamic = 1;
+		}
+		else if (p_type == ELF_PROG_LOAD && p_memsz) {
 			if (p_align % PGSIZE)
 				goto fail;
 			if (p_offset % PGSIZE != p_va % PGSIZE)
 				goto fail;
 
 			uintptr_t filestart = ROUNDDOWN(p_offset, PGSIZE);
-			uintptr_t fileend = p_offset + p_filesz;
-			uintptr_t filesz = fileend - filestart;
+			uintptr_t filesz = p_offset + p_filesz - filestart;
 
 			uintptr_t memstart = ROUNDDOWN(p_va, PGSIZE);
-			uintptr_t memend = ROUNDUP(p_va + p_memsz, PGSIZE);
-			uintptr_t memsz = memend - memstart;
-			if (memend > ei->highest_addr)
-				ei->highest_addr = memend;
-			/* This needs to be a PRIVATE mapping, and the stuff after the file
-			 * needs to be zeroed. */
+			uintptr_t memsz = ROUNDUP(p_va + p_memsz, PGSIZE) - memstart;
+			memstart += pgoffset * PGSIZE;
+
+			if (memstart + memsz > ei->highest_addr)
+				ei->highest_addr = memstart + memsz;
+
+			/* TODO: figure out proper permissions from the elf */
+			int perms = PROT_READ | PROT_WRITE | PROT_EXEC;
+
 			if (filesz) {
-				/* TODO: figure out proper permissions from the elf */
-				if (do_mmap(p, memstart + pgoffset * PGSIZE, filesz,
-				           PROT_READ|PROT_WRITE|PROT_EXEC, MAP_FIXED|MAP_PRIVATE,
-				           f, filestart) == MAP_FAILED)
-					goto fail;
-				/* Due to elf-ghetto-ness, we need to zero the first part of the
-				 * BSS from the last page of the data segment.  We translate to
-				 * the KVA so we don't need to worry about using the proc's
-				 * mapping */
-				uintptr_t z_s = memstart + pgoffset * PGSIZE + filesz;
-				pte_t *pte = pgdir_walk(p->env_pgdir, (void*)z_s, 0);
-				assert(pte);
-				uintptr_t kva_z_s = (uintptr_t)ppn2kva(PTE2PPN(*pte)) + PGOFF(z_s);
-				uintptr_t kva_z_e = ROUNDUP(kva_z_s, PGSIZE);
-				memset((void*)kva_z_s, 0, kva_z_e - kva_z_s);
-				filesz = ROUNDUP(filesz, PGSIZE);
+				/* Due to elf-ghetto-ness, we need to zero the first part of
+				 * the BSS from the last page of the data segment.  If we end
+				 * on a partial page, we map it in separately with
+				 * MAP_POPULATE so that we can zero the rest of it now. We
+				 * translate to the KVA so we don't need to worry about using
+				 * the proc's mapping */
+				uintptr_t partial = PGOFF(filesz);
+
+				if (filesz - partial) {
+					/* Map the complete pages. */
+					if (do_mmap(p, memstart, filesz - partial, perms, flags,
+					            f, filestart) == MAP_FAILED)
+						goto fail;
+				}
+
+				if (partial) {
+					/* Map the final partial page. */
+					uintptr_t last_page = memstart + filesz - partial;
+					int partial_flags = flags | MAP_POPULATE;
+					if (do_mmap(p, last_page, PGSIZE, perms, partial_flags,
+					            f, filestart + filesz - partial) == MAP_FAILED)
+						goto fail;
+
+					/* Zero the end of it. */
+					pte_t *pte = pgdir_walk(p->env_pgdir, (void*)last_page, 0);
+					assert(pte);
+					void* last_page_kva = ppn2kva(PTE2PPN(*pte));
+					memset(last_page_kva + partial, 0, PGSIZE - partial);
+
+					filesz = ROUNDUP(filesz, PGSIZE);
+				}
 			}
 			/* Any extra pages are mapped anonymously... (a bit weird) */
 			if (filesz < memsz)
-				if (do_mmap(p, memstart + filesz + pgoffset*PGSIZE, memsz-filesz,
-				           PROT_READ|PROT_WRITE|PROT_EXEC, MAP_FIXED|MAP_ANON,
-				           NULL, 0) == MAP_FAILED)
+				if (do_mmap(p, memstart + filesz, memsz-filesz,
+				           perms, flags, NULL, 0) == MAP_FAILED)
 					goto fail;
 		}
 	}
 	/* map in program headers anyway if not present in binary.
 	 * useful for TLS in static programs. */
 	if (ei->phdr == -1) {
-		void *phdr_addr = do_mmap(p, MMAP_LOWEST_VA, PGSIZE, PROT_READ, 0, f,
-		                          0);
+		uintptr_t filestart = ROUNDDOWN(e_phoff, PGSIZE);
+		uintptr_t filesz = e_phoff + (e_phnum * phsz) - filestart;
+		void *phdr_addr = do_mmap(p, 0, filesz, PROT_READ,
+		                          flags & ~MAP_FIXED, f, filestart);
 		if (phdr_addr == MAP_FAILED)
 			goto fail;
 		ei->phdr = (long)phdr_addr + e_phoff;
@@ -135,22 +159,24 @@ static int load_one_elf(struct proc *p, struct file *f, int pgoffset,
 	ei->elf64 = elf64;
 	ret = 0;
 fail:
-	kfree(elf);
+	if (phdrs)
+	  kfree(phdrs);
+	current = cur_proc;
 	return ret;
 }
 
 int load_elf(struct proc* p, struct file* f)
 {
 	elf_info_t ei, interp_ei;
-	if (load_one_elf(p, f, 0,& ei))
+	if (load_one_elf(p, f, 0, &ei))
 		return -1;
 
 	if (ei.dynamic) {
 		struct file *interp = do_file_open(ei.interp, 0, 0);
 		if (!interp)
 			return -1;
-		/* careful, this could conflict with the mmap from the TLS up above */
- 		int error = load_one_elf(p, interp, 2, &interp_ei);
+		/* Load dynamic linker one page into the address space */
+		int error = load_one_elf(p, interp, 1, &interp_ei);
 		kref_put(&interp->f_kref);
 		if (error)
 			return -1;
@@ -181,7 +207,6 @@ int load_elf(struct proc* p, struct file* f)
 	proc_init_trapframe(&p->env_tf,0,core0_entry,USTACKTOP);
 	p->env_entry = ei.entry;
 
-	// map in stack using POPULATE (because SPARC requires it)
 	int flags = MAP_FIXED | MAP_ANONYMOUS;
 	#ifdef __sparc_v8__
 	flags |= MAP_POPULATE; // SPARC stacks must be mapped in
