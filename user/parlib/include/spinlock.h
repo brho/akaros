94baf19693896c6d4db24cd3c5922bc20967df71
Kevin Klues <klueska@cs.berkeley.edu>
2012-12-14 14:45:09 -0800

    Add userspace spinlock implementation

---
+++ user/parlib/include/spinlock.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2011 The Regents of the University of California
+ * Barret Rhoden <brho@cs.berkeley.edu>
+ * Kevin Klues <klueska@cs.berkeley.edu>
+ *
+ * This file is part of Parlib.
+ * 
+ * Parlib is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Parlib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ * 
+ * See COPYING.LESSER for details on the GNU Lesser General Public License.
+ * See COPYING for details on the GNU General Public License.
+ */
+
+#ifndef SPINLOCK_H
+#define SPINLOCK_H
+
+#include <arch/arch.h>
+#include <arch/atomic.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define SPINLOCK_LOCKED ((atomic_t)(1))
+#define SPINLOCK_UNLOCKED ((atomic_t)(0))
+
+typedef atomic_t spinlock_t;
+
+void spinlock_init(spinlock_t *lock);
+int spinlock_trylock(spinlock_t *lock);
+void spinlock_lock(spinlock_t *lock);
+void spinlock_unlock(spinlock_t *lock);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif // SPINLOCK_H
---
+++ user/parlib/spinlock.c
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2011 The Regents of the University of California
+ * Barret Rhoden <brho@cs.berkeley.edu>
+ * Kevin Klues <klueska@cs.berkeley.edu>
+ *
+ * This file is part of Parlib.
+ * 
+ * Parlib is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Parlib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Lesser GNU General Public License for more details.
+ * 
+ * See COPYING.LESSER for details on the GNU Lesser General Public License.
+ * See COPYING for details on the GNU General Public License.
+ */
+
+#include <stdlib.h>
+#include <errno.h>
+#include <assert.h>
+#include "spinlock.h"
+
+void spinlock_init(spinlock_t *lock)
+{
+  assert(lock);
+  *lock = SPINLOCK_UNLOCKED;
+}
+
+
+int spinlock_trylock(spinlock_t *lock) 
+{
+  assert(lock);
+  if (*lock == SPINLOCK_LOCKED)
+    return EBUSY;
+
+  return (int)atomic_cas(lock, (long)SPINLOCK_LOCKED, (long)SPINLOCK_UNLOCKED);
+}
+
+
+void spinlock_lock(spinlock_t *lock) 
+{
+  assert(lock);
+  while (spinlock_trylock(lock) != (int)SPINLOCK_UNLOCKED)
+    cpu_relax();
+}
+
+
+void spinlock_unlock(spinlock_t *lock) 
+{
+  assert(lock);
+  *lock = SPINLOCK_UNLOCKED;
+}
