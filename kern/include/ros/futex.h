928e92bd0c5b8702e8b8103144b07137e9a0ae2a
Barret Rhoden <brho@cs.berkeley.edu>
2012-12-19 12:06:35 -0800

    Ifdefs out the kernel from futex.h
    I'm not a fan of having a file in the kernel headers that is not part of
    our kernel interface.

--- kern/include/ros/futex.h
+++ kern/include/ros/futex.h
@@ -1,3 +1,7 @@
+/* This isn't really part of the kernel interface, and is just placed here so
+ * gcc can find it during its compilation. */
+#ifndef ROS_KERNEL
+
 #ifndef _FUTEX_H
 #define _FUTEX_H
 
@@ -12,3 +16,5 @@ int futex(int *uaddr, int op, int val, const struct timespec *timeout,
                  int *uaddr2, int val3);
 
 #endif	/* _FUTEX_H */
+
+#endif /* ifndef ROS_KERNEL */
