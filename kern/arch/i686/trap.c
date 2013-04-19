brho/akaros

commit 0a19848d01420aebfefdae1685f8f3055b3d9f8a

Removes unnecessary memset() from pop_ros_tf
Was adding quite a bit (~22ns) to context switch overhead.

user/parlib/include/i686/vcore.h
@@ -79,7 +79,6 @@ static inline void pop_ros_tf(struct user_trapframe *tf, uint32_t vcoreid)
 	rst->notif_disab_loc = (uint32_t)&vcpd->notif_disabled;
 	rst->notif_pend_loc = (uint32_t)&vcpd->notif_pending;
 	rst->sysc = &rst->local_sysc;	/* point to the local one */
-	memset(rst->sysc, 0, sizeof(struct syscall));
 	/* Need to prep the async sysc in case we need to notify ourselves */
 	rst->sysc->num = SYS_self_notify;
 	rst->sysc->arg0 = vcoreid;	/* arg 1 & 2 already = 0 (null notif, no u_ne)*/

tests/pthread_test.c
@@ -71,6 +71,10 @@ int main(int argc, char** argv)
 		pthread_can_vcore_request(FALSE);	/* 2LS won't manage vcores */
 		pthread_lib_init();					/* gives us one vcore */
 		vcore_request(nr_vcores - 1);		/* ghetto incremental interface */
+		for (int i = 0; i < nr_vcores; i++) {
+			printf("Vcore %d mapped to pcore %d\n", i,
+			       __procinfo.vcoremap[i].pcoreid);
+		}
 	}
 
 	/* create and join on yield */

commit ffb43da7ee066ae9729f43f1012ec7c68ff81da5

Fixes bug with pop_ros_ctx (x86)
Again, much like 2612bab3, removing the memset in 0a19848d0 caused
another bug, where we were no longer sending in EV_NONE and a null
message.  Having a gibberish message led to sending very high event
numbers, which was basically a quasi-random function pointer.

Good times!

kern/arch/i686/trap.c
@@ -555,6 +555,13 @@ void page_fault_handler(struct hw_trapframe *hw_tf)
 		       current->pid, prot & PROT_READ ? "READ" : "WRITE", fault_va,
 		       hw_tf->tf_eip, core_id(), err);
 		print_trapframe(hw_tf);
+		/* Turn this on to help debug bad function pointers */
+		printd("esp %08p\n\t 0(esp): %08p\n\t 4(esp): %08p\n\t 8(esp): %08p\n"
+		       "\t12(esp): %08p\n", hw_tf->tf_esp,
+		       *(uintptr_t*)(hw_tf->tf_esp +  0),
+		       *(uintptr_t*)(hw_tf->tf_esp +  4),
+		       *(uintptr_t*)(hw_tf->tf_esp +  8),
+		       *(uintptr_t*)(hw_tf->tf_esp + 12));
 		proc_destroy(current);
 	}
 }

kern/src/syscall.c
@@ -832,9 +832,6 @@ static int sys_self_notify(struct proc *p, uint32_t vcoreid,
                            bool priv)
 {
 	struct event_msg local_msg = {0};
-
-	printd("[kernel] received self notify for vcoreid %d, type %d, msg %08p\n",
-	       vcoreid, ev_type, u_msg);
 	/* if the user provided an ev_msg, copy it in and use that */
 	if (u_msg) {
 		if (memcpy_from_user(p, &local_msg, u_msg, sizeof(struct event_msg))) {
@@ -843,6 +840,12 @@ static int sys_self_notify(struct proc *p, uint32_t vcoreid,
 		}
 	} else {
 		local_msg.ev_type = ev_type;
+	}
+	if (local_msg.ev_type >= MAX_NR_EVENT) {
+		printk("[kernel] received self-notify for vcoreid %d, ev_type %d, "
+		       "u_msg %08p, u_msg->type %d\n", vcoreid, ev_type, u_msg,
+		       u_msg ? u_msg->ev_type : 0);
+		return -1;
 	}
 	/* this will post a message and IPI, regardless of wants/needs/debutantes.*/
 	post_vcore_event(p, &local_msg, vcoreid, priv ? EVENT_VCORE_PRIVATE : 0);

user/parlib/event.c
@@ -179,6 +179,7 @@ int handle_one_mbox_msg(struct event_mbox *ev_mbox)
 	if (get_ucq_msg(&ev_mbox->ev_msgs, &local_msg) == -1)
 		return 0;
 	ev_type = local_msg.ev_type;
+	assert(ev_type < MAX_NR_EVENT);
 	printd("[event] UCQ (mbox %08p), ev_type: %d\n", ev_mbox, ev_type);
 	if (ev_handlers[ev_type])
 		ev_handlers[ev_type](&local_msg, ev_type);
@@ -430,4 +431,15 @@ void ev_we_returned(bool were_handling_remotes)
 {
 	if (were_handling_remotes)
 		__vc_handle_an_mbox = TRUE;
+}
+
+/* Debugging */
+void print_ev_msg(struct event_msg *msg)
+{
+	printf("MSG at %08p\n", msg);
+	printf("\ttype: %d\n", msg->ev_type);
+	printf("\targ1 (16): 0x%4x\n", msg->ev_arg1);
+	printf("\targ2 (32): 0x%8x\n", msg->ev_arg2);
+	printf("\targ3 (32): 0x%8x\n", msg->ev_arg3);
+	printf("\targ4 (64): 0x%16x\n", msg->ev_arg4);
 }

user/parlib/include/event.h
@@ -48,4 +48,7 @@ void try_handle_remote_mbox(void);
 bool ev_might_not_return(void);
 void ev_we_returned(bool were_handling_remotes);
 
+/* Debugging */
+void print_ev_msg(struct event_msg *msg);
+
 #endif /* _EVENT_H */

user/parlib/include/i686/vcore.h
@@ -84,7 +84,9 @@ static inline void pop_user_ctx(struct user_context *ctx, uint32_t vcoreid)
 	rst->sysc->num = SYS_self_notify;
 	rst->sysc->flags = 0;
 	rst->sysc->ev_q = 0;		/* technically not needed but will avoid bugs */
-	rst->sysc->arg0 = vcoreid;	/* arg 1 & 2 already = 0 (null notif, no u_ne)*/
+	rst->sysc->arg0 = vcoreid;
+	rst->sysc->arg1 = EV_NONE;
+	rst->sysc->arg2 = 0;		/* no ev_msg */
 	rst->sysc->arg3 = TRUE;		/* just a private VCPD notification */
 	rst->eax_save = 0;			/* avoid bugs */
 	rst->eflags = tf->tf_eflags;