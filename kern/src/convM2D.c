0b91af7cdd1fdf8ecf1fa1c8f56bb6b8d9c094b1
Ronald G. Minnich <rminnich@gmail.com>
2013-08-12 09:59:16 -0700

    convert functions to akaros-style dirents
    convM2A converts to akaros kdirents, which we hope to make less limiting
    in the future. We might, for example, return more informative dirents
    from the kernel and then cut them down to
    POSIX-style braindead dirents in a library.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

--- kern/src/convM2D.c
+++ kern/src/convM2D.c
@@ -103,3 +103,74 @@ unsigned int
 	
 	return p - buf;
 }
+
+/* Special akaros edition. */
+/* akaros does not (yet) pass as much info as plan 9 does,
+ * and it still has stuff I'm not happy about like an inode number.
+ */
+#if 0
+struct kdirent {
+        __ino64_t                                       d_ino;          /* inod
+e number */
+        __off64_t                                       d_off;          /* offs
+et to the next dirent */
+        unsigned short                          d_reclen;       /* length of th
+is record */
+        unsigned char                           d_type;
+        char                                            d_name[MAX_FILENAME_SZ 
++ 1];   /* filename */
+} __attribute__((aligned(8)));
+
+#endif
+
+unsigned int
+  convM2A(uint8_t *buf, unsigned int nbuf, struct kdirent *kd)
+{
+	uint8_t *p, *ebuf;
+	char *sv[4];
+	int i, ns;
+	uint32_t junk;
+
+	if(nbuf < STATFIXLEN)
+		return 0; 
+
+	p = buf;
+	ebuf = buf + nbuf;
+
+	p += BIT16SZ;	/* ignore size */
+	kd->d_type = GBIT16(p);
+	p += BIT16SZ;
+	junk = GBIT32(p);
+	p += BIT32SZ;
+	junk = GBIT8(p);
+	p += BIT8SZ;
+	junk = GBIT32(p);
+	p += BIT32SZ;
+	kd->d_ino = GBIT64(p);
+	p += BIT64SZ;
+	junk /* mode */= GBIT32(p);
+	p += BIT32SZ;
+	junk /*d->atime*/ = GBIT32(p);
+	p += BIT32SZ;
+	junk /*d->mtime*/ = GBIT32(p);
+	p += BIT32SZ;
+	junk /*d->length */= GBIT64(p);
+	p += BIT64SZ;
+
+
+	/* for now, uids in akaros are ints. Does not
+	 * matter; kdirents are limited in what they tell you.
+	 * get the name, ignore the rest
+	 */
+	if(p + BIT16SZ > ebuf)
+		return 0;
+	/* name size. */
+	ns = GBIT16(p);
+	p += BIT16SZ;
+	if(p + ns > ebuf)
+		return 0;
+	kd->d_reclen = ns;
+	memmove(kd->d_name, p, ns);
+	
+	return p - buf;
+}
