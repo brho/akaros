fbb83a680ed1adf99d0710c98aa61e37a216294b
Ronald G. Minnich <rminnich@gmail.com>
2013-08-23 09:01:16 -0700

    Bind program
    Usage: bind [flags] new old
    e.g.
    bind '#p' '#r/proc'
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

---
+++ tests/bind.c
@@ -0,0 +1,25 @@
+#define _LARGEFILE64_SOURCE /* needed to use lseek64 */
+
+#include <stdio.h> 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <arch/arch.h>
+#include <unistd.h>
+#include <errno.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) 
+{ 
+	int ret;
+	if (argc < 2){
+		fprintf(stderr, "usage: %s new old\n", argv[0]);
+		exit(1);
+	}
+
+	ret = syscall(145, argv[1], argv[2], 0);
+
+	printf("%d\n", ret);
+}
