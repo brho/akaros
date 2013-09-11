44fdb928c9f6b0602531a5a8197347aaa606e9c1
Barret Rhoden <brho@cs.berkeley.edu>
2013-09-11 15:44:28 -0700

    Fixes dev random (newrandom)
    Moves unnecessary globals, and attempts to add pseudo-randomness.
    
    The old bits was getting passed down the pipe, instead of new, and I
    also added in some TSC 'noise'.  Finally, the read-side was losing some
    info with that xor with p.  p tended to be things like
    0x000000000060b620, 0x000000000060b621, 0x000000000060b622, etc.  And it
    was a user-controlled pointer.
    
    And I gave newrandom.c a good-old lindent.

--- kern/drivers/dev/newrandom.c
+++ kern/drivers/dev/newrandom.c
@@ -14,72 +14,81 @@
 #include <schedule.h>
 #include <apipe.h>
 
-struct Rb
-{
-	struct cond_var	producer;
-	struct cond_var	consumer;
-	uint32_t	randomcount;
-	uint8_t	buf[1024];
+#define RAND_BUF_SZ 1024
+
+struct rb {
+	uint8_t buf[RAND_BUF_SZ];
 	struct atomic_pipe ap;
-	uint16_t	bits;
-	uint32_t	randn;
-	uint32_t	next;
-	uint32_t where;
+	uint32_t randn;
 } rb;
 
-static void
-genrandom(uint32_t srcid, long a0, long a1, long a2)
+static void genrandom(uint32_t srcid, long a0, long a1, long a2)
 {
-	for(;;){
-		uint8_t new;
+	unsigned int counter = 0;
+	uint16_t bits = 0;
+	uint32_t where = 0;
+	for (;;) {
+		uint16_t new;
+		/* magic number from ron.  the only thing truly random in here!  Right
+		 * now, all this does is slow down our production of random numbers - we
+		 * don't actually use this timing info for anything. */
 		udelay_sched(772);
-		rb.randomcount = 1;
-		rb.bits = (rb.bits<<2) ^ rb.randomcount;
-		rb.next++;
-		if(rb.next != 8/2)
+		/* the 1 used to be randomcount.  though it was always 1, here and in
+		 * the old plan9 devrandom */
+		bits = (bits << 2) ^ 1;
+		/* only produce a random byte every four wakeups.  not sure why. */
+		counter++;
+		if (counter != 4)
 			continue;
-		rb.next = 0;
-		new = rb.buf[rb.where++ % 1024];
-		new ^= rb.bits;
-		apipe_write(&rb.ap, &rb.bits, 2);
+		counter = 0;
+		/* where indexes our previous entries pushed in the pipe.  we end up
+		 * looking at the first entry we are about to fill (assuming only one
+		 * producer), which means we'll look at the oldest entry in the pipe
+		 * (RAND_BUF_SZ entries ago, in this case).
+		 *
+		 * we use this old byte for the upper half of the two bytes we produce,
+		 * and use the lower byte of the tsc for the lower half. */
+		new = rb.buf[where++ % RAND_BUF_SZ];
+		new <<= 8;
+		new |= read_tsc() & 0xff;
+		new ^= bits;
+		apipe_write(&rb.ap, &new, 2);
 	}
 }
 
-void
-randominit(void)
+void randominit(void)
 {
 	/* Frequency close but not equal to HZ */
 	apipe_init(&rb.ap, rb.buf, sizeof(rb.buf), 1);
-	printk("randominit\n");
-	send_kernel_message(core_id(), genrandom, 0, 0, 0,
-	                    KMSG_ROUTINE);
+	send_kernel_message(core_id(), genrandom, 0, 0, 0, KMSG_ROUTINE);
 }
 
 /*
  *  consume random bytes from a circular buffer
  */
-uint32_t
-randomread(void *xp, uint32_t n)
+uint32_t randomread(void *xp, uint32_t n)
 {
 	ERRSTACK(2);
 	uint8_t *e, *p;
 	uint32_t x;
 	int amt;
 	p = xp;
-	if(waserror()){
+	if (waserror()) {
 		nexterror();
 	}
 
-	for(e = p + n; p < e; ){
-		amt = apipe_read(&rb.ap,p,1);
+	for (e = p + n; p < e;) {
+		amt = apipe_read(&rb.ap, p, 1);
 		if (amt < 0)
 			error("randomread apipe");
+		/* This is all bullshit, and we really don't have any randomness in the
+		 * system.  This is some leftover hacked stuff from plan9. */
 		/*
 		 *  beating clocks will be predictable if they are
 		 *  synchronized.  Use a cheap pseudo random number
 		 *  generator to obscure any cycles.
 		 */
-		x = rb.randn*1103515245 ^ (uintptr_t)p;
+		x = (rb.randn + 1) * 1103515245;
 		*p++ = rb.randn = x;
 
 	}
