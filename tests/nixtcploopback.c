cf99763a7df494fb02c9177b64b54c9e4442d793
Barret Rhoden <brho@cs.berkeley.edu>
2013-10-10 22:16:40 -0700

    Removes tcploopback debugging
    If we need it back for one-off debugging, just reverse this commit.
    
    We do still need to have the caller sync and wait on the server to
    announce (or try in a loop, yielding, til it is up, etc).

--- tests/nixtcploopback.c
+++ tests/nixtcploopback.c
@@ -12,27 +12,18 @@
 void caller(char *addr)
 {
 	char data[512];
-	printf("Caller %d dialing %s\n", getpid(), addr);
 	int fd = dial(addr, 0, 0, 0);
-	printf("Caller %d, dialed %s and got %d\n", getpid(), addr, fd);
 	while (1){
 		int amt;
-		printf("Caller about to read\n");
 		amt = read(0, data, sizeof(data));
 		if (amt <= 0)
 			break;
-		printf("Caller read %s, about to write it to %d\n", data, fd);
 		amt = write(fd, data, amt);
-		printf("Caller wrote, got %d back.  will read from %d\n", amt, fd);
 		amt = read(fd, data, sizeof(data));
-		printf("Caller read %d bytes from %d\n", amt, fd);
 		if (amt <= 0)
 			break;
 		write(1, data, amt);
 	}
-	printf("Caller spinning\n");
-	while (1)
-		sleep(10);
 }
 
 int server(char *addr)
@@ -42,41 +33,30 @@ int server(char *addr)
 	int n;
 	char buf[256];
 	acfd = announce(addr, adir);
-	printf("\tServer announced, got acfd %d\n", acfd);
 	if (acfd < 0)
 		return -1;
 	for (;;) {
 /* listen for a call */
-		printf("\tServer listening\n", lcfd);
 		lcfd = listen(adir, ldir);
-		printf("\tServer listened, got lcfd %d\n", lcfd);
 		if (lcfd < 0)
 			return -1;
 /* fork a process to echo */
 		switch (fork()) {
 			case -1:
-				printf("\tServer fork failed\n");
 				perror("forking");
 				exit(1);
 				break;
 			case 0:
-				printf("\t\tserver forked, in child %d\n", getpid());
 /* accept the call and open the data file */
 				dfd = accept(lcfd, ldir);
-				printf("\t\tserver child %d accept dfd %d\n", getpid(), dfd);
 				if (dfd < 0)
 					return -1;
 /* echo until EOF */
-				printf("\t\tserver child %d about to read\n", getpid());
 				while ((n = read(dfd, buf, sizeof(buf))) > 0) {
-					printf("\t\tserver child %d read %d, about to write\n",
-					       getpid(), n);
 					write(dfd, buf, n);
-					printf("\t\tserver child %d about to read\n", getpid());
 				}
 				exit(0);
 			default:
-				printf("\tServer parent %d, closing lcdf %d\n", getpid(), lcfd);
 				close(lcfd);
 				break;
 		}
@@ -101,6 +81,7 @@ void main(int argc, char **argv)
 		exit(1);
 	}
 	if (pid == 0){
+		/* Attempt to get the caller to dial after the server announces. */
 		for (int i = 0; i < 3; i++)
 			sys_block(1000000);
 		caller(caddr);
