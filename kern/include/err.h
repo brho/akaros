eab45e959f3ddf8d10cce1dc63e294c003802e8b
Barret Rhoden <brho@cs.berkeley.edu>
2014-01-16 12:49:01 -0800

    Plan9 style waserror() handling
    Adapted from the latest version we built for the old 9ns branch.
    
    You'll need to #include err.h to use this.  Since it includes some other
    files of its own, I didn't want to put it in common.h.

---
+++ kern/include/err.h
@@ -0,0 +1,32 @@
+/* Plan9 style error popping.  For details, read Documentation/plan9.txt */
+
+#ifndef ROS_KERN_ERR_H
+#define ROS_KERN_ERR_H
+
+#include <setjmp.h>
+#include <syscall.h>
+
+struct errbuf {
+	struct jmpbuf jmpbuf;
+};
+
+/* add 1 in case they forget they need an entry for the passed-in errbuf */
+#define ERRSTACK(x) struct errbuf *prev_errbuf; struct errbuf errstack[(x)];   \
+                    int curindex = 0;
+#define waserror() (errpush(errstack, ARRAY_SIZE(errstack), &curindex,         \
+                            &prev_errbuf) ||                                   \
+                    setjmp(&(get_cur_errbuf()->jmpbuf)))
+#define error(x,...) {set_errstr(x, ##__VA_ARGS__);                            \
+                      longjmp(&get_cur_errbuf()->jmpbuf, 1);}
+#define nexterror() {errpop(errstack, ARRAY_SIZE(errstack), &curindex,         \
+                            prev_errbuf);                                      \
+                     longjmp(&(get_cur_errbuf())->jmpbuf, 1);}
+#define poperror() {errpop(errstack, ARRAY_SIZE(errstack), &curindex,          \
+                           prev_errbuf);}
+
+int errpush(struct errbuf *errstack, int stacksize, int *curindex,
+            struct errbuf **prev_errbuf);
+void errpop(struct errbuf *errstack, int stacksize, int *curindex,
+            struct errbuf *prev_errbuf);
+
+#endif /* ROS_KERN_ERR_H */
---
+++ kern/src/err.c
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2013 Google Inc.
+ */
+//#define DEBUG
+#include <setjmp.h>
+#include <vfs.h>
+#include <kfs.h>
+#include <slab.h>
+#include <kmalloc.h>
+#include <kref.h>
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include <error.h>
+#include <cpio.h>
+#include <pmap.h>
+#include <smp.h>
+#include <err.h>
+
+/* General idea: if we're at the base for this func (base of ERRSTACK in the
+ * scope where ERRSTACK and waserror are used), we need to find and save the
+ * previous errbuf, so we know how to pop back.
+ *
+ * The main goal of this is to track and advertise (via pcpui) the errbuf that
+ * should be jumped to next (when we call error()).  Note that the caller of
+ * this (waserror()) will fill the jumpbuf shortly with its context.
+ *
+ * When we enter, curindex points to the slot we should use.  First time, it is
+ * 0, and we'll set cur_eb to slot 0.  When we leave, curindex is set to the
+ * next free slot. */
+int errpush(struct errbuf *errstack, int stacksize, int *curindex,
+            struct errbuf **prev_errbuf)
+{
+	printd("pushe %p %d %dp\n", errstack, stacksize, *curindex);
+	if (*curindex == 0)
+		*prev_errbuf = get_cur_errbuf();
+
+	if (*curindex >= stacksize)
+		panic("Error stack overflow");
+	set_cur_errbuf(&errstack[*curindex]);
+	*curindex = *curindex + 1;
+	return 0;
+}
+
+/* Undo the work of errpush, and advertise the new errbuf used by error() calls.
+ * We only need to be tricky when we reached the beginning of the stack and need
+ * to check the prev_errbuf from a previous ERRSTACK/function.
+ *
+ * When we enter, curindex is the slot of the next *free* errstack (the one we'd
+ * push into if we were pushing.  When we leave, it will be decreased by one,
+ * and will still point to the next free errstack (the one we are popping).
+ * */
+void errpop(struct errbuf *errstack, int stacksize, int *curindex,
+            struct errbuf *prev_errbuf)
+{
+	printd("pope %p %d %d\n", errstack, stacksize, *curindex);
+	/* curindex now points to the slot we are popping*/
+	*curindex = *curindex - 1;
+	/* We still need to advertise the previous slot, which is one back from
+	 * curindex.  If curindex is 0, that means the next free slot is the first
+	 * of our errstack.  In this case, we need to advertise the prev. */
+	if (*curindex < 0)
+		panic("Error stack underflow");
+
+	if (*curindex == 0)
+		set_cur_errbuf(prev_errbuf);
+	else
+		set_cur_errbuf(&errstack[*curindex - 1]);	/* use the prior slot */
+}
