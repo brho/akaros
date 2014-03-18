ccbdd6c009e2d9ed2c7cb89774cceb8028f76eac
Ronald G. Minnich <rminnich@google.com>
2014-03-18 16:10:17 -0700

    Fix csquery.c
    /net/cs
    
    Signed-off-by: Ronald G. Minnich <rminnich@google.com>

--- tests/csquery.c
+++ tests/csquery.c
@@ -86,7 +86,7 @@ void main(int argc, char **argv)
 	if (argc > 0)
 		server = argv[0];
 	else
-		server = "#s/cs"; // "/net/cs";
+		server = "/net/cs";
 
 	if (argc > 1) {
 		for (i = 1; i < argc; i++)
