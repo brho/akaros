a8b5db37f5d1d8c9e5972289c9ed0dcae71d6ae4
Ronald G. Minnich <rminnich@google.com>
2014-07-25 18:40:17 +0000

    Add a test for the rename system call.
    Maybe we can fix it to use the constant. The build system keeps defeating me.
    
    Signed-off-by: Ronald G. Minnich <rminnich@google.com>

---
+++ tests/testrename.c
@@ -0,0 +1,49 @@
+/* Copyright (C) 1991, 1995, 1996, 1997 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <assert.h>
+#include <net.h>
+#include <errno.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+/* Rename the file OLD to NEW.  */
+int
+rename ( const char *old, const char *new)
+{
+	syscall(123 /*SYS_rename*/, old, strlen(old), new, strlen(new), 0, 0);
+}
+
+int main(int argc, char *argv[])
+{
+	int ret;
+	if (argc != 3) {
+		fprintf(stderr, "arg count. Moron\n");
+		exit(1);
+	}
+
+	ret = rename(argv[1], argv[2]);
+	return ret;
+}
