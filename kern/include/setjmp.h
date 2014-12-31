28d2570dd0833e9356f05475ca8354ad1435ddaa
Barret Rhoden <brho@cs.berkeley.edu>
2014-12-30 22:47:24 -0500

    Disable -Wmaybe-uninitialized for setjmp
    Gcc 4.9.2 complains.
    
    Setting bool err = 0 or something doesn't matter btw; the actual complaint is
    for an anonymous variable.  The returns twice might be confusing, or maybe that
    the compiler can't see the code for slim_setjmp.  The usages of slim_setjmp in
    ASM looks fine (immediately tests eax).

--- kern/include/setjmp.h
+++ kern/include/setjmp.h
@@ -8,9 +8,14 @@
 int slim_setjmp(struct jmpbuf *env) __attribute__((returns_twice));
 void longjmp(struct jmpbuf *env, int val) __attribute__((noreturn));
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+
 #define setjmp(jb) ({bool err;                                                 \
                     __ros_clobber_callee_regs();                               \
                     err = slim_setjmp(jb);                                     \
                     err;})
+
+#pragma GCC diagnostic pop
 
 #endif /* !ROS_SETJMP_H */
