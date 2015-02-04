6efd296854fc4ec0aef749b744f3c376fe2e20c8
Barret Rhoden <brho@cs.berkeley.edu>
2015-02-04 13:40:36 -0500

    BNX2X: Semaphore compatibility
    Going with #defines, to keep down_interruptible and down_timeout.  Might need
    those in the future.

--- kern/drivers/net/bnx2x/akaros_compat.h
+++ kern/drivers/net/bnx2x/akaros_compat.h
@@ -5,9 +5,6 @@
 #ifndef ROS_KERN_AKAROS_COMPAT_H
 #define ROS_KERN_AKAROS_COMPAT_H
 
-#define __rcu
-typedef unsigned long dma_addr_t;
-
 /* Common headers that most driver files will need */
 
 #include <assert.h>
@@ -23,5 +20,20 @@ typedef unsigned long dma_addr_t;
 #include <bitmap.h>
 #include <mii.h>
 #include <umem.h>
+
+#define __rcu
+typedef unsigned long dma_addr_t;
+
+#define DEFINE_SEMAPHORE(name)  \
+    struct semaphore name = SEMAPHORE_INITIALIZER_IRQSAVE(name, 1)
+#define sema_init(sem, val) sem_init_irqsave(sem, val)
+#define up(sem) sem_up(sem)
+#define down(sem) sem_down(sem)
+#define down_trylock(sem) ({!sem_trydown(sem);})
+/* In lieu of spatching, I wanted to keep the distinction between down and
+ * down_interruptible/down_timeout.  Akaros doesn't have the latter. */
+#define down_interruptible(sem) ({sem_down(sem); 0;})
+#define down_timeout(sem, timeout) ({sem_down(sem); 0;})
+
 
 #endif /* ROS_KERN_AKAROS_COMPAT_H */
