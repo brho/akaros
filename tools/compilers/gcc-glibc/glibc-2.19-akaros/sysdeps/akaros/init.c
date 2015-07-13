c7f31ea8662e0409adeccc87e1aff360f133816b
Kevin Klues <klueska@cs.berkeley.edu>
2015-07-12 23:31:12 -0700

    Finalize arg, env, aux migration (2/3) (CXX) (BB)
    In this commit I remove a few sysdep files that we no longer need to
    specialize anymore. However, because Akaros needs to have the ability to
    reenter the _start function mutliple times, it is impossible to do away
    with all sysdeps on these low level interfaces.
    
    The true system dependent stuff has now been isolated in a file called
    init.c in sysdeps/akaros (this name is required in order to get it to
    link in with crt1.o).  It now introduces a new function called
    __libc_vcore_entry(), which handles all the code necessary to deal with
    our reentrant _start() function.  Our system dependant _start() function
    simply calls out to __libc_vcore_entry() and then jumps to the
    _real_start(), as defined by the current architecture (currently only
    x86_64).
    
    By becoming compliant with the SYSV ABI all of the code dealing with
    unpacking our args, env, and auxv data from procinfo is now unnecessary.

---
+++ tools/compilers/gcc-glibc/glibc-2.19-akaros/sysdeps/akaros/init.c
@@ -0,0 +1,75 @@
+#include <abort-instr.h>
+#include <string.h>
+#include <parlib/vcore.h>
+#include <stdio.h>
+#include <ros/syscall.h>
+#include <ros/procinfo.h>
+#include <unistd.h>
+#include <vcore-tls.c>
+#include <ctype.h>
+
+__thread int __vcoreid = 0;
+__thread bool __vcore_context = FALSE;
+
+#define failmsg(str) write(2,str"\n",sizeof(str"\n")-1)
+
+void __attribute__((noreturn)) __uthread_vcore_entry(void)
+{
+	fputs("Define a uthread_vcore_entry() or a vcore_entry(), foo!\n", stderr);
+	abort();
+	__builtin_unreachable();
+}
+weak_alias(__uthread_vcore_entry, uthread_vcore_entry)
+
+void __attribute__((noreturn)) __vcore_entry(void)
+{
+	uthread_vcore_entry();
+	fputs("vcore_entry() should never return!\n", stderr);
+	abort();
+	__builtin_unreachable();
+}
+weak_alias(__vcore_entry, vcore_entry)
+
+void __libc_vcore_entry(int id)
+{
+	static int init = 0;
+	/* For dynamically-linked programs, the first time through,
+	 * __vcore_self_on_entry could be clobbered (on x86), because the linker
+	 * will have overwritten eax.  Happily, the first time through, we know we
+	 * are vcore 0.  Subsequent entries into this routine do not have this
+	 * problem. */
+	if (init == 0)
+		id = 0;
+
+	/* vcore0 when it comes up again, and all threads besides thread 0 must
+	 * acquire a TCB. */
+	if (init || (id != 0)) {
+		/* The kernel sets the TLS desc for us, based on whatever is in VCPD.
+		 *
+		 * x86 32-bit TLS is pretty jacked up, so the kernel doesn't set the TLS
+		 * desc for us.  it's a little more expensive to do it here, esp for
+		 * amd64.  Can remove this when/if we overhaul 32 bit TLS.
+		 *
+		 * AFAIK, riscv's TLS changes are really cheap, and they don't do it in
+		 * the kernel (yet/ever), so they can set their TLS here too. */
+		#ifndef __x86_64__
+		set_tls_desc((void*)__procdata.vcore_preempt_data[id].vcore_tls_desc,
+		             id);
+		#endif
+		/* These TLS setters actually only need to happen once, at init time */
+		__vcoreid = id;
+		__vcore_context = TRUE;
+		__ctype_init();	/* set locale info for ctype functions */
+		vcore_entry();
+		failmsg("why did vcore_entry() return?");
+		abort();
+		#ifdef ABORT_INSTRUCTION
+		ABORT_INSTRUCTION;
+		#endif
+
+		while(1);
+		__builtin_unreachable();
+	}
+	init = 1;
+}
+
