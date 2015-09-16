47e4ca19290b7ef80f5c49a3f8870c7d4c729c76
Xiao Jia <stfairy@gmail.com>
2015-09-16 16:16:02 -0700

    Import errno.h from Linux 4.1
    The file is actually a result of concatenating multiple files to avoid
    pulling in the whole directory structure of Linux.  The file is simply
    a series of #define's, so there's no need to spatch it.
    
    [brho: edited to use ros/errno.h for the common #defines]

---
+++ kern/include/linux/errno.h
@@ -0,0 +1,34 @@
+#ifndef _LINUX_ERRNO_H
+#define _LINUX_ERRNO_H
+
+#include <ros/errno.h>
+
+#define ERFKILL		132	/* Operation not possible due to RF-kill */
+#define EHWPOISON	133	/* Memory page has hardware error */
+
+/*
+ * These should never be seen by user programs.  To return one of ERESTART*
+ * codes, signal_pending() MUST be set.  Note that ptrace can observe these
+ * at syscall exit tracing, but they will never be left for the debugged user
+ * process to see.
+ */
+#define ERESTARTSYS	512
+#define ERESTARTNOINTR	513
+#define ERESTARTNOHAND	514	/* restart if no handler.. */
+#define ENOIOCTLCMD	515	/* No ioctl command */
+#define ERESTART_RESTARTBLOCK 516 /* restart by calling sys_restart_syscall */
+#define EPROBE_DEFER	517	/* Driver requests probe retry */
+#define EOPENSTALE	518	/* open found a stale dentry */
+
+/* Defined for the NFSv3 protocol */
+#define EBADHANDLE	521	/* Illegal NFS file handle */
+#define ENOTSYNC	522	/* Update synchronization mismatch */
+#define EBADCOOKIE	523	/* Cookie is stale */
+#define ENOTSUPP	524	/* Operation is not supported */
+#define ETOOSMALL	525	/* Buffer or request is too small */
+#define ESERVERFAULT	526	/* An untranslatable error occurred */
+#define EBADTYPE	527	/* Type not supported by server */
+#define EJUKEBOX	528	/* Request initiated, but will not complete before timeout */
+#define EIOCBQUEUED	529	/* iocb queued, will get completion event */
+
+#endif
