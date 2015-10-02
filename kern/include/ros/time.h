ff1e66754e7f06fdcbdaf77eb1ec9a7f13160688
Barret Rhoden <brho@cs.berkeley.edu>
2015-10-02 10:14:04 -0400

    Untangling the glibc header rat's nest (P.2) (XCC)
    This time around, we'll try and fix up the time headers.  Ideally,
    anything that is part of the kernel interface is exposed via the kernel
    headers.  For instance, struct timespec and maybe struct timeval (which
    is just mentioned in a comment).
    
    It's relatively easy to override timeval - that's what bits/time.h is
    for (it gets included by sys/time.h).  However, timespec is a little
    trickier, since it's defined directly in time.h, not bits/time.h that is
    made to be sysdep-overrode.  (Note that timespec is available to
    programs via #include time.h.  Timeval comes from sys/time.h...).
    Overriding time.h required the same file twice:
    sysdeps/akaros/time/time.h and sysdeps/akaros/time.h.
    
    With this dive into the rats nest, our kernel header no longer needs the
    mysterious #define __timespec_defined.
    
    Rats' nest ref: 5b5cc8856
    
    Rebuild glibc.

--- kern/include/ros/time.h
+++ kern/include/ros/time.h
@@ -1,12 +1,6 @@
 #ifndef ROS_INC_TIME_H
 #define ROS_INC_TIME_H
 
-/* When userspace includes this file, some part of glibc might have already
- * defined timespec.  It's a rat's nest. */
-#ifndef __timespec_defined
-/* Tells glibc we've covered timespec. */
-#define __timespec_defined 1
-
 /* (newlib) Time Value Specification Structures, P1003.1b-1993, p. 261 */
 typedef long time_t; /* TODO: this is fucked.  Thanks POSIX. */
 
@@ -19,7 +13,5 @@ struct timeval {
 	time_t tv_sec;	/* seconds */
 	time_t tv_usec;	/* microseconds */
 };
-
-#endif /* __timespec_defined */
 
 #endif /* ROS_INC_TIME_H */
--- tests/timer_deps.c
+++ tests/timer_deps.c
@@ -1,9 +1,13 @@
 #include <ros/time.h>
 #include <time.h>
+#include <sys/time.h>
 #include <stdio.h>
 
 int main(int argc, char **argv)
 {
 	printf("sizeof timespec: %d\n", sizeof(struct timespec));
+	printf("sizeof itimerspec: %d\n", sizeof(struct itimerspec));
+	printf("sizeof timeval: %d\n", sizeof(struct timeval));
+	printf("sizeof timezone: %d\n", sizeof(struct timezone));
 	return 0;
 }
