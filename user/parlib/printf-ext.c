5025b06858b52dcd0a536e76f7da6e6e74c59589
Ronald G. Minnich <rminnich@gmail.com>
2015-10-27 08:31:50 -0700

    Add the Inferno license to files we got from Inferno
    This is long overdue. We just kept forgetting. But somebody in
    Harvey wanted one of our files and at that point it's essential
    to get this right.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>
    [minor touchups, added UCB and Google modifications]
    Signed-off-by: Barret Rhoden <brho@cs.berkeley.edu>

--- user/parlib/printf-ext.c
+++ user/parlib/printf-ext.c
@@ -2,16 +2,38 @@
  * Barret Rhoden <brho@cs.berkeley.edu>
  * See LICENSE for details.
  *
- * Parts Copyright //INFERNO
- *
  * Common printf format extensions.  For now, %r is installed by default
  * (in early init code), and the others need to be requested.
  *
  * To register, for example %i for ipaddr, call:
  * 		register_printf_specifier('i', printf_ipaddr, printf_ipaddr_info);
  *
- * __printf_ipaddr, printf_ipmask, and printf_ethaddr adapted from INFERNO's
- * eipconvtest.c. */
+ * __printf_ipaddr, printf_ipmask, and printf_ethaddr adapted from Inferno's
+ * eipconvtest.c.  Their copyright:
+ *
+ * Copyright © 1994-1999 Lucent Technologies Inc.  All rights reserved.
+ * Portions Copyright © 1997-1999 Vita Nuova Limited
+ * Portions Copyright © 2000-2007 Vita Nuova Holdings Limited
+ *                                (www.vitanuova.com)
+ * Revisions Copyright © 2000-2007 Lucent Technologies Inc. and others
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE. */
 
 #include <parlib/printf-ext.h>
 #include <stdio.h>
