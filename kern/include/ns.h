9995f1fe493fcd9a569e45b29b870f00258356ed
Davide Libenzi <dlibenzi@google.com>
2015-11-01 19:05:29 -0800

    Removed lingering error string variables
    Removed lingering error string variables, leftovers after the migration
    to the new error handling code.
    
    Signed-off-by: Davide Libenzi <dlibenzi@google.com>
    Signed-off-by: Barret Rhoden <brho@cs.berkeley.edu>

--- kern/include/ns.h
+++ kern/include/ns.h
@@ -915,49 +915,6 @@ int64_t nhgetv(void *);
 uint32_t nhgetl(void *);
 uint16_t nhgets(void *);
 
-/* error messages, from inferno emu error.h */
-extern char Emount[];			/* inconsistent mount */
-extern char Eunmount[];			/* not mounted */
-extern char Eunion[];			/* not in union */
-extern char Emountrpc[];		/* mount rpc error */
-extern char Enocreate[];		/* mounted directory forbids creation */
-extern char Enonexist[];		/* file does not exist */
-extern char Eexist[];			/* file already exists */
-extern char Enotdir[];			/* not a directory */
-extern char Eisdir[];			/* file is a directory */
-extern char Ebadchar[];			/* bad character in file name */
-extern char Efilename[];		/* file name syntax */
-extern char Eperm[];			/* permission denied */
-extern char Ebadusefd[];		/* inappropriate use of fd */
-extern char Ebadarg[];			/* bad arg in system call */
-extern char Einuse[];			/* device or object already in use */
-extern char Eio[];				/* i/o error */
-extern char Etoobig[];			/* read or write too large */
-extern char Etoosmall[];		/* read or write too small */
-extern char Enoifc[];			/* bad interface or no free interface slots */
-extern char Ehungup[];			/* i/o on hungup channel */
-extern char Ebadctl[];			/* bad process or channel control request */
-extern char Enodev[];			/* no free devices */
-extern char Enochild[];			/* no living children */
-extern char Enovmem[];			/* out of memory: virtual memory */
-extern char Eisstream[];		/* seek on a stream */
-extern char Etimedout[];		/* connection timed out */
-extern char Econrefused[];		/* connection refused */
-extern char Econinuse[];		/* connection in use */
-extern char Eintr[];			/* interrupted */
-extern char Enomem[];			/* out of memory: kernel */
-extern char Eshort[];			/* i/o count too small */
-extern char Enobitstore[];		/* out of screen memory */
-extern char Egreg[];			/* jim'll fix it */
-extern char Ebadspec[];			/* bad attach specifier */
-extern char Enoattach[];		/* mount/attach disallowed */
-extern char Eshortstat[];		/* stat buffer too small */
-extern char Enegoff[];			/* negative i/o offset */
-extern char Ebadstat[];			/* malformed stat buffer */
-extern char Ecmdargs[];			/* wrong #args in control message */
-extern char Enofd[];			/* no free file descriptors */
-extern char Eprocdied[];		/* process died */
-
 char *get_cur_genbuf(void);
 
 /* hack for now. */
