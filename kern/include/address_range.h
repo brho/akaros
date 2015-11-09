948a651ee7a537e7347d735ab6b0a85abeecea7a
Davide Libenzi <dlibenzi@google.com>
2015-11-09 15:10:44 -0800

    Added address range library utility to be able to express whitelists
    Added address range library utility to be able to express whitelists for
    MSR code, and potentially anything which can be expressed as address
    range (ie, PCI, DMA, ...).
    
    Signed-off-by: Davide Libenzi <dlibenzi@google.com>
    Signed-off-by: Barret Rhoden <brho@cs.berkeley.edu>

---
+++ kern/include/address_range.h
@@ -0,0 +1,21 @@
+/* Copyright (c) 2015 Google Inc
+ * Davide Libenzi <dlibenzi@google.com>
+ * See LICENSE for details.
+ */
+
+#pragma once
+
+#include <sys/types.h>
+#include <stdio.h>
+
+#define ADDRESS_RANGE(s, e) { .start = (s), .end = (e) }
+
+struct address_range {
+	uintptr_t start;
+	uintptr_t end;
+};
+
+int address_range_validate(const struct address_range *ars, size_t count);
+int address_range_init(struct address_range *ars, size_t count);
+const struct address_range *address_range_find(const struct address_range *ars,
+											   size_t count, uintptr_t addr);
---
+++ kern/lib/address_range.c
@@ -0,0 +1,55 @@
+/* Copyright (c) 2015 Google Inc
+ * Davide Libenzi <dlibenzi@google.com>
+ * See LICENSE for details.
+ */
+
+#include <ros/errno.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <sort.h>
+#include <address_range.h>
+
+static int address_range_cmp(const void *f1, const void *f2)
+{
+	return ((const struct address_range *) f1)->start <
+		((const struct address_range *) f2)->start ? -1 : 1;
+}
+
+int address_range_validate(const struct address_range *ars, size_t count)
+{
+	for (size_t i = 0; i < count; i++) {
+		if (ars[i].start > ars[i].end)
+			return -EINVAL;
+		if ((i > 0) && (ars[i - 1].end >= ars[i].start))
+			return -EINVAL;
+	}
+
+	return 0;
+}
+
+int address_range_init(struct address_range *ars, size_t count)
+{
+	sort(ars, count, sizeof(*ars), address_range_cmp);
+
+	return address_range_validate(ars, count);
+}
+
+const struct address_range *address_range_find(const struct address_range *ars,
+											   size_t count, uintptr_t addr)
+{
+	ssize_t l = 0, r = count - 1;
+
+	while (l <= r) {
+		ssize_t x = l + (r - l) / 2;
+		const struct address_range *car = ars + x;
+
+		if (car->end < addr)
+			l = x + 1;
+		else if (car->start > addr)
+			r = x - 1;
+		else
+			return car;
+	}
+
+	return NULL;
+}
