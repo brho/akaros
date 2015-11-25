2fa42319139e4cc5ca853546363f84443d0ead00
Dan Cross <crossd@gmail.com>
2015-11-25 17:34:40 -0500

    Rename 'reallocarray' to 'kreallocarray'.
    ...as it should have been named originally.
    My oversight: apologies for that.
    
    Signed-off-by: Dan Cross <crossd@gmail.com>
    Signed-off-by: Barret Rhoden <brho@cs.berkeley.edu>

--- kern/src/kreallocarray.c
+++ kern/src/kreallocarray.c
@@ -26,7 +26,7 @@ enum {
 	MUL_NO_OVERFLOW = ((size_t)1 << (sizeof(size_t) * 4))
 };
 
-void *reallocarray(void *optr, size_t nmemb, size_t size, int flags)
+void *kreallocarray(void *optr, size_t nmemb, size_t size, int flags)
 {
 	if (((nmemb >= MUL_NO_OVERFLOW) || (size >= MUL_NO_OVERFLOW)) &&
 	    (nmemb > 0) && ((SIZE_MAX / nmemb) < size)) {
