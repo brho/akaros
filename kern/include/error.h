23616f789424da373b764cbd07c8b72051a489df
Barret Rhoden <brho@cs.berkeley.edu>
2016-02-13 15:57:08 -0500

    Remove kernel errno string processing
    The kernel doesn't really need to know about the string names for errno
    values.  We were using that mostly as a hack to not use proper
    errstrings.
    
    I kept parse_errno.sh around, since we (theoretically) still use that to
    generate error lists in glibc.
    
    Signed-off-by: Barret Rhoden <brho@cs.berkeley.edu>

--- kern/include/error.h
+++ kern/include/error.h
@@ -5,21 +5,9 @@
 #include <ros/errno.h>
 
 typedef int error_t;
-extern const char *const errno_strings[];
-extern const int MAX_ERRNO;
+
+#define MAX_ERRNO		4095
 
 #define ERR_PTR(err)  ((void *)((intptr_t)(err)))
 #define PTR_ERR(ptr)  ((intptr_t)(ptr))
 #define IS_ERR(ptr)   (-(intptr_t)(ptr) <= MAX_ERRNO)
-
-/* Plan9 wants to return non-const char* all over the place, so even if a const
- * char* would have made much more sense, unless we want to refactor a huge
- * amount of code, we need to return a char*.
- */
-static inline char *errno_to_string(int error)
-{
-	extern const char *const errno_strings[];
-
-	return error >= 0 && error < MAX_ERRNO && errno_strings[error] != NULL ?
-		(char *) errno_strings[error]: (char *) "Unknown error";
-}
