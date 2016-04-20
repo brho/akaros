c8b9e78b753018956aba7f5c6edf12003f786045
Ronald G. Minnich <rminnich@gmail.com>
2016-04-20 11:09:26 -0700

    Add getifaddrs support and a test.
    Per the man page, we don't have to fill out addr, mask, and
    broadaddr, and this is such a dump API that we don't.
    
    Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>
    [closed net and addr on error]
    Signed-off-by: Barret Rhoden <brho@cs.berkeley.edu>

---
+++ tests/getifaddrs.c
@@ -0,0 +1,45 @@
+/* This file is part of the UCB release of Plan 9. It is subject to the license
+ * terms in the LICENSE file found in the top-level directory of this
+ * distribution and at http://akaros.cs.berkeley.edu/files/Plan9License. No
+ * part of the UCB release of Plan 9, including this file, may be copied,
+ * modified, propagated, or distributed except according to the terms contained
+ * in the LICENSE file. */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <parlib/parlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <iplib/iplib.h>
+#include <iplib/icmp.h>
+#include <ctype.h>
+#include <pthread.h>
+#include <parlib/spinlock.h>
+#include <parlib/timing.h>
+#include <parlib/tsc-compat.h>
+#include <parlib/printf-ext.h>
+#include <benchutil/alarm.h>
+#include <ndblib/ndb.h>
+#include <ifaddrs.h>
+
+int main(int argc, char **argv)
+{
+	int i, naddr, o;
+	uint8_t *cp;
+	struct ifaddrs *ifa;
+
+	naddr = getifaddrs(&ifa);
+
+	for (naddr = 0; ifa; ifa = ifa->ifa_next, naddr++) {
+		printf("%s: ", ifa->ifa_name);
+		cp = ifa->ifa_data;
+		for (o = 0; o < 6; o++) {
+			printf("%02x", cp[o]);
+			if (o < 5)
+				printf(":");
+		}
+		printf("\n");
+	}
+	printf("%d ifaddrs\n", naddr);
+	freeifaddrs(ifa);
+}
