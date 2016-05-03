239e31ad0a62e71ad645b960587d428ed7df272b
Michael Taufen <mtaufen@gmail.com>
2016-05-03 15:06:34 -0700

    VMM: Remove the old virtio (XCC)
    Signed-off-by: Michael Taufen <mtaufen@gmail.com>
    Signed-off-by: Barret Rhoden <brho@cs.berkeley.edu>

--- user/vmm/include/vmm/virtio_rng.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-/* This header is BSD licensed so anyone can use the definitions to implement
- * compatible drivers/servers. */
-#include <linux/virtio_ids.h>
-#include <linux/virtio_config.h>
