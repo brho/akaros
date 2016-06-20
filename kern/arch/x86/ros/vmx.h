1141ed6ce69b489ddaf03ba89e4ceca952b1e985
Barret Rhoden <brho@cs.berkeley.edu>
2016-06-20 16:38:44 -0400

    VMM: Touch up ros/vmx.h's includes (XCC)
    We need those includes to compile.  I ran into this while making a new C
    file.
    
    We also need to get rid of the rdmsr macros, since userspace should never
    even call that.  Right now, you could get a compilation error with those
    macros since read_msr() isn't in any user headers.
    
    Reinstall your kernel headers
    
    Signed-off-by: Barret Rhoden <brho@cs.berkeley.edu>

--- kern/arch/x86/ros/vmx.h
+++ kern/arch/x86/ros/vmx.h
@@ -23,6 +23,9 @@
  */
 #pragma once
 
+#include <ros/common.h>
+#include <ros/arch/mmu.h>
+
 #define CPU_BASED_VIRTUAL_INTR_PENDING  0x00000004
 #define CPU_BASED_USE_TSC_OFFSETING     0x00000008
 #define CPU_BASED_HLT_EXITING           0x00000080
