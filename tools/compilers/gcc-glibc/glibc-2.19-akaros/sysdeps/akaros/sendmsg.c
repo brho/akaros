741c47c853d91bd95916dde5bab3455ee44efef5
Barret Rhoden <brho@cs.berkeley.edu>
2016-09-02 14:58:05 -0400

    Implement sendmsg() (XCC)
    This doesn't do any fancy control operations - just the iovec sendto().
    
    Rebuild glibc.
    
    Signed-off-by: Barret Rhoden <brho@cs.berkeley.edu>

---
+++ tools/compilers/gcc-glibc/glibc-2.19-akaros/sysdeps/akaros/sendmsg.c
@@ -0,0 +1,22 @@
+/* Copyright (c) 2015-2016 Google Inc.
+ * Barret Rhoden <brho@cs.berkeley.edu>
+ * See LICENSE for details.
+ *
+ * sendmsg(), on top of sendto(). */
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+
+/* In sendto.c */
+ssize_t __sendto_iov(int fd, const struct iovec *iov, int iovcnt,
+                     int flags, __CONST_SOCKADDR_ARG to, socklen_t tolen);
+
+/* Send a message described MSG on socket FD.  Returns the number of bytes
+ * sent, or -1 for errors.  */
+ssize_t __sendmsg(int fd, const struct msghdr *msg, int flags)
+{
+	return __sendto_iov(fd, msg->msg_iov, msg->msg_iovlen, flags, msg->msg_name,
+	                    msg->msg_namelen);
+}
+weak_alias(__sendmsg, sendmsg)
