brho/akaros

commit 27f971ff2d4e4498cc0beead91f497ce1696acab

strace: Fix issues with a few syscalls
SYS_getcwd wasn't part of the file set.  We were tracing the wrong
arguments for SYS_chdir and SYS_rmdir.  SYS_readlink had an unnecessary
check on retval.

Signed-off-by: Barret Rhoden <brho@cs.berkeley.edu>

kern/src/syscall.c
@@ -344,8 +344,6 @@ static void systrace_start_trace(struct kthread *kthread, struct syscall *sysc)
 		copy_tracedata_from_user(trace, sysc->arg1, sysc->arg2);
 		break;
 	case SYS_openat:
-	case SYS_chdir:
-	case SYS_rmdir:
 	case SYS_nmount:
 		copy_tracedata_from_user(trace, sysc->arg1, sysc->arg2);
 		break;
@@ -353,7 +351,9 @@ static void systrace_start_trace(struct kthread *kthread, struct syscall *sysc)
 	case SYS_lstat:
 	case SYS_access:
 	case SYS_unlink:
+	case SYS_chdir:
 	case SYS_mkdir:
+	case SYS_rmdir:
 	case SYS_wstat:
 		copy_tracedata_from_user(trace, sysc->arg0, sysc->arg1);
 		break;
@@ -441,9 +441,7 @@ static void systrace_finish_trace(struct kthread *kthread, long retval)
 				break;
 			copy_tracedata_from_user(trace, trace->arg0, trace->arg1);
 			snprintf_to_trace(trace, " -> ");
-			copy_tracedata_from_user(trace, trace->arg2,
-			                         (int)trace->retval < 0 ? 0
-									                        : trace->retval);
+			copy_tracedata_from_user(trace, trace->arg2, retval);
 			break;
 		}
 	}

tests/strace.c
@@ -84,6 +84,7 @@ static struct trace_set path_trace_set = { "path",
 	 SYS_symlink,
 	 SYS_readlink,
 	 SYS_chdir,
+	 SYS_getcwd,
 	 SYS_mkdir,
 	 SYS_rmdir,
 	 SYS_nbind,
@@ -132,6 +133,7 @@ static struct trace_set file_trace_set = { "file",
 	 SYS_symlink,
 	 SYS_readlink,
 	 SYS_chdir,
+	 SYS_getcwd,
 	 SYS_mkdir,
 	 SYS_rmdir,
 	 SYS_nbind,

commit 10640d4ec945a6de9cf7a3f32392a756b908b42d

strace: Fix SYS_chdir data parsing
I broke this a few commits back.  Our chdir syscall takes a pid for the
first argument, unlike the man page for chdir.

Fixes: 27f971ff2d4e ("strace: Fix issues with a few syscalls")

Signed-off-by: Barret Rhoden <brho@cs.berkeley.edu>

kern/src/syscall.c
@@ -264,6 +264,7 @@ static void systrace_start_trace(struct kthread *kthread, struct syscall *sysc)
 		copy_tracedata_from_user(trace, sysc->arg1, sysc->arg2);
 		break;
 	case SYS_openat:
+	case SYS_chdir:
 	case SYS_nmount:
 		copy_tracedata_from_user(trace, sysc->arg1, sysc->arg2);
 		break;
@@ -271,7 +272,6 @@ static void systrace_start_trace(struct kthread *kthread, struct syscall *sysc)
 	case SYS_lstat:
 	case SYS_access:
 	case SYS_unlink:
-	case SYS_chdir:
 	case SYS_mkdir:
 	case SYS_rmdir:
 	case SYS_wstat: