b1a393406cb6b77dcd911b3ebb21471b0837ccf9
Barret Rhoden <brho@cs.berkeley.edu>
2019-03-27 18:01:02 -0400

    Set up git blame to ignore the "8-space-tab commits"
    If you're using git hyper-blame, the .git-blame-ignore-revs file should
    work; that is their default.
    
    If you're using my patches for upstream Git, the git config settings
    will work.  If the interface for my upstream stuff changes, or an
    alternate version gets implemented, I'll update the script.
    
    Signed-off-by: Barret Rhoden <brho@cs.berkeley.edu>

--- scripts/one-time-setup.sh
+++ scripts/one-time-setup.sh
@@ -11,3 +11,9 @@ then
 	echo Error: you need to set AKAROS_ROOT
 	exit -1
 fi
+
+# This feature may not be upstream yet - you can use brho's git if it isn't.
+# This tells blame to ignore certain commits know to be uninteresting.
+
+git config --local --replace-all blame.ignorerevsfile .git-blame-ignore-revs
+git config --local --replace-all blame.markignoredlines true
