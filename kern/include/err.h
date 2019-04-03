26e4d0b9a3ef64c609f7ac331984aaea9f9b799f
Barret Rhoden <brho@cs.berkeley.edu>
2019-04-03 15:21:15 -0400

    Clean up and clarify slim_setjmp() / waserror()
    Any time you use a variable inside a waserror() block that has been
    written since the waserror() / setjmp(), that variable must be marked
    volatile.
    
    The bool err; in slim_setjmp served little purpose, other than
    confusion.
    
    Signed-off-by: Barret Rhoden <brho@cs.berkeley.edu>

--- kern/include/err.h
+++ kern/include/err.h
@@ -9,7 +9,7 @@
 #define ERRSTACK(x) struct errbuf *prev_errbuf; struct errbuf errstack[(x)];   \
                     int curindex = 0;
 #define waserror() setjmp(&(errpush(errstack, ARRAY_SIZE(errstack), &curindex, \
-									&prev_errbuf)->jmpbuf))
+				    &prev_errbuf)->jmpbuf))
 
 /* In syscall.h, but want to avoid circular include issues. */
 extern void set_error(int error, const char *fmt, ...);
--- kern/include/setjmp.h
+++ kern/include/setjmp.h
@@ -8,11 +8,10 @@ int slim_setjmp(struct jmpbuf *env) __attribute__((returns_twice));
 void longjmp(struct jmpbuf *env, int val) __attribute__((noreturn));
 
 #pragma GCC diagnostic push
+/* Currently, this only throws in tcpackproc().  Not sure why, but if you take
+ * out the loop++ > 1000, it won't warn. */
 #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
 
-#define setjmp(jb) ({bool err;                                                 \
-                    __ros_clobber_callee_regs();                               \
-                    err = slim_setjmp(jb);                                     \
-                    err;})
+#define setjmp(jb) ({ __ros_clobber_callee_regs(); slim_setjmp(jb); })
 
 #pragma GCC diagnostic pop
