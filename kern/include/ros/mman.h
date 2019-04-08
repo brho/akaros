e816d5c0ec933196f0cf16fa133a246483f82bda
Barret Rhoden <brho@cs.berkeley.edu>
2019-04-08 19:41:01 -0400

    mm: remove unused MAP_ and PROT_ flags (XCC)
    mmap() and mprotect() have a bunch of flags that we don't use, such as
    PROT_GROWSDOWN and MAP_DENYWRITE.  These only existed in the kernel
    interface, yet we never actually use them.
    
    This commit removes those flags from the kernel interface.  Userspace
    can still pass them to us - they can pass anything after all.  We won't
    honor them and may throw errors if we get them.  I'd rather not export
    definitions for the kernel interface that the kernel doesn't know about.
    
    Note that glibc still has these common flags in
    glibc-2.19-akaros/sysdeps/akaros/bits/mman.h, which is Linux's glibc
    header.  Replacing that outright at the moment needs some work.  For
    instance, malloc calls __madvise() and whatnot.
    
    Reinstall your kernel headers.
    
    Signed-off-by: Barret Rhoden <brho@cs.berkeley.edu>

--- kern/include/ros/mman.h
+++ kern/include/ros/mman.h
@@ -15,10 +15,6 @@
 #define PROT_WRITE		0x2
 #define PROT_EXEC		0x4
 #define PROT_NONE		0x0
-#define PROT_GROWSDOWN		0x01000000
-#define PROT_GROWSUP		0x02000000
-// TODO NOT A REAL STATE
-#define PROT_UNMAP		0x100
 
 /* mmap flags, only anonymous is supported now, feel free to pass others */
 #define MAP_SHARED		0x01
@@ -27,17 +23,7 @@
 #define MAP_ANONYMOUS		0x20
 #define MAP_ANON MAP_ANONYMOUS
 
-#define MAP_GROWSDOWN		0x00100
-#define MAP_DENYWRITE		0x00800
-#define MAP_EXECUTABLE		0x01000
 #define MAP_LOCKED		0x02000
-#define MAP_NORESERVE		0x04000
 #define MAP_POPULATE		0x08000
-#define MAP_NONBLOCK		0x10000
-#define MAP_STACK		0x20000
 
 #define MAP_FAILED		((void*)-1)
-
-/* Other mmap flags, which we probably won't support
-#define MAP_32BIT
-*/
